6dd8e9ec9aa45e74c94fda5b527c16e267a6f9f7
Oliver-makes-code <oliver@olivermakesco.de>
2025-12-15 12:53:25 -0600

    Coerce values to string

--- ruleconf/src/main/java/gay/vulpines/ruleconf/RuleConf.java
+++ ruleconf/src/main/java/gay/vulpines/ruleconf/RuleConf.java
@@ -1,14 +1,11 @@
 package gay.vulpines.ruleconf;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.FileReader;
-import java.io.IOException;
 
 public class RuleConf implements ModInitializer {
     private static final Gson GSON = new Gson();
@@ -18,15 +15,28 @@ public class RuleConf implements ModInitializer {
 
         if (path.exists()) {
             try (var reader = new FileReader(path)) {
-                return GSON.fromJson(reader, JsonObject.class);
-            } catch (JsonParseException e) {
+                JsonObject raw = GSON.fromJson(reader, JsonObject.class);
+                JsonObject result = new JsonObject();
+
+                for (var entry : raw.entrySet()) {
+                    var value = entry.getValue();
+
+                    if (value.isJsonPrimitive())
+                        result.addProperty(entry.getKey(), value.getAsString());
+                    else
+                        result.addProperty(entry.getKey(), value.toString());
+                }
+
+                return result;
+            } catch (Exception e) {
                 e.printStackTrace();
-            } catch (IOException ignored) {}
+            }
         }
 
         return new JsonObject();
     }
 
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+    }
 }
--- ruleconf/src/main/java/gay/vulpines/ruleconf/mixin/Mixin_GameRules.java
+++ ruleconf/src/main/java/gay/vulpines/ruleconf/mixin/Mixin_GameRules.java
@@ -4,33 +4,21 @@ import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.DynamicLike;
 import com.mojang.serialization.JsonOps;
 import gay.vulpines.ruleconf.RuleConf;
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.level.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
 
 @Mixin(GameRules.class)
 public abstract class Mixin_GameRules {
     @Shadow protected abstract void loadFromTag(DynamicLike<?> dynamicLike);
 
     @Inject(
-            method = "<init>(Ljava/util/Map;Lnet/minecraft/world/flag/FeatureFlagSet;)V",
+            method = "<init>*",
             at = @At("RETURN")
     )
-    private void modifyRules(Map<GameRules.Key<?>, GameRules.Value<?>> map, FeatureFlagSet featureFlagSet, CallbackInfo ci) {
-        loadFromTag(new Dynamic<>(JsonOps.INSTANCE, RuleConf.getRuleMap()));
-    }
-
-    @Inject(
-            method = "<init>(Lnet/minecraft/world/flag/FeatureFlagSet;Lcom/mojang/serialization/DynamicLike;)V",
-            at = @At("RETURN")
-    )
-    private void modifyRules(FeatureFlagSet featureFlagSet, DynamicLike<?> dynamicLike, CallbackInfo ci) {
+    private void injectRules() {
         loadFromTag(new Dynamic<>(JsonOps.INSTANCE, RuleConf.getRuleMap()));
     }
 }
